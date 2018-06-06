6a1caa73022fec99c462100cca1a892ddc089164
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-06-06 12:59:45 -0500

    Componente app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EventsComponent } from './events/events.component';
+
+import { FormsModule } from '@angular/forms';
+import { DescriptionComponent } from './description/description.component'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+    DescriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
