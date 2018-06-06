e5835583976ef446cdadb195c46d26b7a21735fd
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-06-06 12:58:16 -0500

    Mock eventos (pruebas offline - tutorial)

---
+++ src/app/mock-events.ts
@@ -0,0 +1,8 @@
+import {Event} from './models/models';
+
+// export const EVENTS: Event[] = [
+//     { ID: "f1f2f3", Name: 'Event 1' },
+//     { ID: "e1e2e3", Name: 'Event 2' },
+//     { ID: "m1m2m3", Name: 'Event 3' },
+//     { ID: "z1z2z3", Name: 'Event 4' }
+// ];
