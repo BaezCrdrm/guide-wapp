ab0d1026797f7df618cdd7ecd2db698f77a2089f
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-06-06 13:00:18 -0500

    componente eventos

---
+++ src/app/events/events.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { Event } from '../models/models';
+import { EventsService } from '../events.service';
+
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrls: ['./events.component.css']
+})
+export class EventsComponent implements OnInit {
+  // event:Event = new Event();
+  // events = EVENTS;
+  events: Event[];
+  selectedEvent: Event;
+  config;
+  constructor(private eventService: EventsService) {  }
+
+  ngOnInit() {
+    this.getEventsList();
+    // this.showConfig();
+  }
+
+  getEventsList():void{
+    // Convierte Observable a "lista"
+    this.eventService.getEventsList().subscribe(events => this.events = events);
+  }
+
+  // showConfig() {
+  //   this.eventService.getConfig()
+  //     .subscribe((data) => this.config = {
+  //         heroesUrl: data['heroesUrl'],
+  //         textfile:  data['textfile']
+  //     });
+  // }
+
+  onSelectEvent(event:Event): void
+  {
+    this.selectedEvent = event;
+  }
+}
